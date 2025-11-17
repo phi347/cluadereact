4dacec9db73fc0c8c864cab6e35db1cede8c1958
phi347 <thisisphpc@gmail.com>
2025-11-17 03:56:37 +0330

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Get the repository name from environment variables if running in CI,
+// or set it manually for local development if needed.
+const repoName = process.env.firstreact || 'firstreact'; // Replace 'your-repo-name'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${firstreact}/`, // Set the base path
+  // If you are deploying to a custom domain, you might set base to '/'
+  // base: '/',
 })
